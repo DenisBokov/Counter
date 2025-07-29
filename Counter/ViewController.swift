979ba27ccf3d722e92facf8517e8e2d6619fa585
Den <practicum@ya.ru>
2025-07-29 10:58:08 +0300

    Немного чистоты кода

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,15 +9,15 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet private weak var countLabel: UILabel!
     
-    @IBOutlet weak var historyChangesTextView: UITextView!
+    @IBOutlet private weak var historyChangesTextView: UITextView!
     
-    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet private weak var plusButton: UIButton!
     
-    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
     
-    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet private weak var clearButton: UIButton!
     
     private var counter: Int = 0
     
@@ -32,6 +32,15 @@ final class ViewController: UIViewController {
 
     }
     
+    private func changeAppearanceButtons(_ buttons: UIButton...) {
+        buttons.forEach { $0.layer.cornerRadius = 10 }
+    }
+    
+    private func updateTextViewAndLabel(for text: String, and labelText: String) {
+        historyChangesTextView.text += text
+        countLabel.text = labelText
+    }
+    
     @IBAction private func increaseCounter() {
         let date = Date().formatToString(for: .dateFormatter)
         counter += 1
@@ -44,7 +53,6 @@ final class ViewController: UIViewController {
             counter -= 1
             updateTextViewAndLabel(for: "\(date): значение изменено на -1\n", and: "\(counter)")
         } else {
-            counter = 0
             historyChangesTextView.text += "\(date): попытка уменьшить значение счётчика ниже 0\n"
         }
     }
@@ -53,15 +61,6 @@ final class ViewController: UIViewController {
         let date = Date().formatToString(for: .dateFormatter)
         counter = 0
         updateTextViewAndLabel(for: "\(date): значение сброшено\n", and: "\(counter)")
-    }
-    
-    private func changeAppearanceButtons(_ buttons: UIButton...) {
-        buttons.forEach { $0.layer.cornerRadius = 10 }
-    }
-    
-    private func updateTextViewAndLabel(for text: String, and labelText: String) {
-        historyChangesTextView.text += text
-        countLabel.text = labelText
     }
 }
 
